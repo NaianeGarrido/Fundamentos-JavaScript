151ddcbc7e659742b5c680505de9b1ca82a9fd33
NaianeGarrido <naiane.ng@hotmail.com>
2022-03-08 13:52:41 -0300

    exemplos aula1

---
+++ booleans/type-booleans.js
@@ -0,0 +1,12 @@
+// False
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero === segundoNumero);
+
+
+// true
+const primeiroNumero1 = 5;
+const segundoNumero1 = 5;
+
+console.log(primeiroNumero1 === segundoNumero1);
---
+++ strings/type-string.js
@@ -0,0 +1,8 @@
+const texto1 = "olá mundo!"
+const senha = "senhaSegura123!"
+const citacao = 'Meu nome é';
+const MeuNome = ' Naiane'
+
+// concatenação (+) somar texto
+
+console.log(citacao + MeuNome)
